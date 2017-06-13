cc7f823b7d1b9ada74ccd7aa24b02d5ad1e64656
1 <lulin1@xiaomi.com>
2017-06-13 14:20:05 +0800

    Use images.weserv.nl to adapt zhihu image url && optimize the css of Newslist.vue

--- vue-zhihu/src/components/NewsList.vue
+++ vue-zhihu/src/components/NewsList.vue
@@ -2,10 +2,8 @@
   <div class="news-list">
     <ul class="list" ref="NewsList">
       <li class="list-item" v-for="story in this.$store.state.stories" >
-         <span>{{story.title}}</span>
-         <div>
-           <img v-bind:src="story.images[0]" :alt="story.title" />
-         </div>
+          <span class="story-title">{{story.title}}</span>
+          <img class="story-img" v-bind:src="attachImageUrl(story.images[0])" :alt="story.title" />
       </li>
     </ul>
   </div>
@@ -43,7 +41,14 @@ export default {
       .catch(error => {
         console.log(error);
       });
-    }
+    },
+
+    attachImageUrl: function(srcUrl) {
+        if (srcUrl !== undefined) {
+          return srcUrl.replace(/http\w{0,1}:\/\/p/g, 'https://images.weserv.nl/?url=p');
+        }
+    },
+
     }
 }
 
@@ -61,11 +66,36 @@ ul {
 }
 
 li {
-  display: inline-block;
+  display: block;
   margin: 0 10px;
 }
 
 a {
   color: #42b983;
 }
+
+.list-item{
+  height: 200px;
+  border-bottom: 3px solid #f5f5f5;
+  text-align: left;
+}
+
+.story-title {
+  height: 200px;
+  width: 70%;
+  /*line-height: 200px;*/
+  vertical-align: middle;
+  font-size: 40px;
+  float: left;
+  margin-left: 50px;
+  margin-top: 5px;
+}
+
+.story-img {
+  vertical-align: middle;
+  width: 20%;
+  height: 150px;
+  float: right;
+}
+
 </style>
