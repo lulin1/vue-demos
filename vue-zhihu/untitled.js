78702fce892e0bf8b036d578c5a264aa9c0fe1dc
1 <lulin1@xiaomi.com>
2017-06-19 17:20:35 +0800

    Delete useless file

--- vue-zhihu/untitled.js
+++
@@ -1,5 +0,0 @@
-intA=prompt("请输入第一个数字："," ");
-intB=prompt("请输入第二个数字：",);
-document.write("你输入的第一个数字是："+intA);
-document.write("你输入的第二个数字是："+intB);
-document.write("<br>两者和是：</br>"+(parseInt(intA)+parseInt(intB)));
