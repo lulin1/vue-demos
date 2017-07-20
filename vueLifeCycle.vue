7ce077099ee89ef8990a322d2ee989dba054098c
1 <lulin1@xiaomi.com>
2017-07-20 16:19:23 +0800

    Add vueLifeCycle.html

--- vueLifeCycle.vue
+++ vueLifeCycle.vue
@@ -6,51 +6,51 @@
 
 <script>
 var myVue = new Vue({          
-	el: "#app",          
-	data: {
-		a: "Vue.js"        
-	},         
-	beforeCreate: function() {        
-		console.log("创建前")            
-		console.log(this.a)            
-		console.log(this.$el)          
-	},         
-	created: function() {
-		console.log("创建之后");            
-		console.log(this.a)            
-		console.log(this.$el)          
-	},         
-	beforeMount: function() {            
-		console.log("mount之前")            
-		console.log(this.a)            
-		console.log(this.$el)          
-	},          
-	mounted: function() {            
-		console.log("mount之后")            
-		console.log(this.a)            
-		console.log(this.$el)          
-	},          
-	beforeUpdate: function() {            
-		console.log("更新前");            
-		console.log(this.a)            
-		console.log(this.$el)          
-	},          
-	updated: function() {            
-		console.log("更新完成");            
-		console.log(this.a);            
-		console.log(this.$el)          
-	},          
-	beforeDestroy: function() {            
-		console.log("销毁前");            
-		console.log(this.a)            
-		console.log(this.$el)      
-	},          
-	destroyed: function() {           
-		console.log("已销毁");          
-		console.log(this.a)          
-		console.log(this.$el)          
-	}   
-  });  
+  el: "#app",          
+  data: {
+    a: "Vue.js"        
+  },         
+  beforeCreate: function() {        
+    console.log("创建前")            
+    console.log(this.a)            
+    console.log(this.$el)          
+  },         
+  created: function() {
+    console.log("创建之后");            
+    console.log(this.a)            
+    console.log(this.$el)          
+  },         
+  beforeMount: function() {            
+    console.log("mount之前")            
+    console.log(this.a)            
+    console.log(this.$el)          
+  },          
+  mounted: function() {            
+    console.log("mount之后")            
+    console.log(this.a)            
+    console.log(this.$el)          
+  },          
+  beforeUpdate: function() {            
+    console.log("更新前");            
+    console.log(this.a)            
+    console.log(this.$el)          
+  },          
+  updated: function() {            
+    console.log("更新完成");            
+    console.log(this.a);            
+    console.log(this.$el)          
+  },          
+  beforeDestroy: function() {            
+    console.log("销毁前");            
+    console.log(this.a)            
+    console.log(this.$el)      
+  },          
+  destroyed: function() {           
+    console.log("已销毁");          
+    console.log(this.a)          
+    console.log(this.$el)          
+  }   
+});  
 </script>
 
 <style>
