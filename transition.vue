2fad6d71a1b4d13937dba2613211f716c0a90a3c
1 <lulin1@xiaomi.com>
2017-08-01 15:25:28 +0800

    Delete vue-music

---
+++ transition.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="demo">
+	  <button v-on:click="show = !show">
+	    Toggle
+	  </button>
+	  <transition name="fade">
+	    <p v-if="show">hello</p>
+	  </transition>
+	</div>
+</template>
+
+<script>
+new Vue({
+  el: '#demo',
+  data: {
+    show: true
+  }
+})
+</script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in below version 2.1.8 */ {
+  opacity: 0
+}
+</style>
