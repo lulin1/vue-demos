934008727d1b7587697668cbf789488e9f4ee51e
1 <lulin1@xiaomi.com>
2017-08-02 11:23:25 +0800

    Init Transition.vue in vue-grammer-demos and delete autoOpenBrowser

---
+++ vue-grammer-demos/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <transition></transition>
+  </div>
+</template>
+
+<script>
+import Transition from './components/Transition'
+
+export default {
+  name: 'app',
+  components: {
+    Transition
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-grammer-demos/src/components/Transition.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="demo">
+    <button v-on:click="show = !show">
+      Toggle
+    </button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      show: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .9s
+}
+
+.fade-enter, .fade-leave-to /* .fade-leave-active in below version 2.1.8 */ {
+  opacity: 0
+}
+
+</style>
