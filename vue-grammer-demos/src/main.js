05b2ecf98d77303cdabd6840240a6fca1fe1bbff
1 <lulin1@xiaomi.com>
2017-08-08 16:05:41 +0800

    Delete useless space

--- vue-grammer-demos/src/main.js
+++ vue-grammer-demos/src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import VueParticles from 'vue-particles'
 
-Vue.use(VueParticles)  
+Vue.use(VueParticles)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
